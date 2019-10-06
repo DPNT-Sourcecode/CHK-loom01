a4a4cd29e368e0e827fbe0ff1ed6f41cf1f27d46
sbx_user1051 <sbx_user1051@169.254.148.77>
2019-10-06 13:24:18 +0000

    Sun Oct 06 13:24:18 UTC 2019

--- src/test/java/befaster/solutions/HLO/HloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HloSolutionTest.java
@@ -1,5 +1,22 @@
 package befaster.solutions.HLO;
 
-public class HloSolutionTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class HloSolutionTest {
+	private HelloSolution helloSolution;
+	
+	@Before
+	public void setUp() {
+		this.helloSolution = new HelloSolution();
+	}
+	
+	@Test
+    public void get_hello() {
+        assertThat(helloSolution.hello("Tom"), equalTo("Hello, Tom!"));
+    }
 }
+
