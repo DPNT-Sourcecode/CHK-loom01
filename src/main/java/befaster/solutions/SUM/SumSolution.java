867b2d78243e0df933d7a39bd02a348ee05fed32
sbx_user1051 <sbx_user1051@169.254.148.77>
2019-10-06 13:00:46 +0000

    Sun Oct 06 13:00:46 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -5,8 +5,17 @@ import befaster.runner.SolutionNotImplementedException;
 @SuppressWarnings("unused")
 public class SumSolution {
 
-    public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+    public int compute(int x, int y) throws Exception {
+    	if (x < 0 || x > 100) {
+    		throw new Exception("Wrong value provided.");
+    	}
+    	
+    	if (y < 0 || y > 100) {
+    		throw new Exception("Wrong value provided.");
+    	}
+    	
+        return x + y;
     }
 
 }
+
