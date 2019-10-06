03c6ca6d863cf6c5ee7fbf8d10f711bad2f606f0
sbx_user1051 <sbx_user1051@169.254.148.77>
2019-10-06 13:18:17 +0000

    Sun Oct 06 13:18:17 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -2,12 +2,9 @@ package befaster.solutions.HLO;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        if (friendName == null) {
-        	throw new RuntimeException("Wrong value provided");
-        }
-        
-        return "Hello world, " + friendName;
+        return "Hello, World!";
     }
 }
+
 
 
