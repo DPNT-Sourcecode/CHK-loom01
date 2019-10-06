fb432d8cfcc970ab59aeda43a26d24156e099752
sbx_user1051 <sbx_user1051@169.254.148.77>
2019-10-06 13:38:15 +0000

    Sun Oct 06 13:38:15 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,15 +1,23 @@
 package befaster.solutions.CHK;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutSolution {
 	
 	private Map<String, Integer> itemsPriceMap = new HashMap<>();
+	private Map<String, Map<Integer, Integer>> specialOffersMap = new HashMap<>();
+	
+	public CheckoutSolution() {
+		itemsPriceMap.put("A", 50);
+		itemsPriceMap.put("B", 30);
+		itemsPriceMap.put("C", 20);
+		itemsPriceMap.put("D", 15);
+	}
 	
     public Integer checkout(String skus) {
         
     }
 }
+
 
